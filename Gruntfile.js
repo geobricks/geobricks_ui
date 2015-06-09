6587c09267736b9f3198151f70cf0de2d20cb65f
Kalimaha <guido.barbaglia@gmail.com>
2015-06-09 11:15:43 +0200

    As a developer I want to edit a single file to configure the application. closes #9

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    var schema_dir = 'resources/schemas/';
+    var output_dir = 'config/';
+
+    /* Initiate Grunt configuration. */
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json')
+    });
+
+    /* Create configuration file. */
+    grunt.registerTask('default', 'Create JSON configuration file from JSON Schema.', function() {
+
+        /* Load JSON Schema utilities. */
+        var defaults = require('json-schema-defaults');
+
+        /* Load JSON Schema. */
+        var schema = grunt.file.readJSON(schema_dir + 'configuration_schema.json', [, {
+            encoding: 'utf8'
+        }]);
+
+        /* Create JSON. */
+        var json = defaults(schema);
+
+        /* Write JSON. */
+        grunt.file.write(output_dir + 'geobricks.json', JSON.stringify(json), [, {encoding: 'utf8'}]);
+
+    });
+
+};
